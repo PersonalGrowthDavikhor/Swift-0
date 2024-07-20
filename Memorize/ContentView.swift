67f299c9232eeb9ea970af28a4a3774a9ea629d8
David <david.chircea@gmail.com>
2024-07-20 15:27:15 +0200

    Card creation

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,13 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack{
+            CardView()
+            CardView(isFaceUp: true)
         }
+        .foregroundColor(.purple)
         .padding()
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                //fill or stroke border, but not both
+                //we're doing this for the card to be well seen in dark mode too
+                RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+                    .fill(Color.white)
+                RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+                    .strokeBorder(lineWidth: 3)
+                //dashed border with 10 on, 2 off
+                /*RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+                 .strokeBorder(style: StrokeStyle(lineWidth: 10, dash: [10,2]))
+                 */
+                Text("🤓").font(.largeTitle)
+            }
+            else {
+                RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+            }
+            
+        }
     }
 }
 
